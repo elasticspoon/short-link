9889c0b016d47f3acff6b0633a317f37fdf10fcd
elasticspoon <mail@yuribocharov.dev>
2025-08-19 18:45:17 -0400

    wip

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,17 @@ require 'rack/test'
 require 'rack'
 require 'digest/sha1'
 require 'rack/request'
+require 'database_cleaner'
+
+# Configure DatabaseCleaner
+DatabaseCleaner.strategy = :transaction
+
+class Minitest::Test
+  def before_setup
+    DatabaseCleaner.start
+  end
+
+  def after_teardown
+    DatabaseCleaner.clean
+  end
+end
